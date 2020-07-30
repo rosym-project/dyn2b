f5c96b73337327a58a808c7d8fd5d892c3745399
Sven Schneider <->
2020-07-30 11:02:19 +0200

    declare kinematic chain structs as extern

--- include/dyn2b/example/robots.h
+++ include/dyn2b/example/robots.h
@@ -8,11 +8,11 @@ extern "C" {
 #endif
 
 
-struct kcc_kinematic_chain one_dof_robot_c;
-struct kca_kinematic_chain one_dof_robot_a;
+extern struct kcc_kinematic_chain one_dof_robot_c;
+extern struct kca_kinematic_chain one_dof_robot_a;
 
-struct kcc_kinematic_chain two_dof_robot_c;
-struct kca_kinematic_chain two_dof_robot_a;
+extern struct kcc_kinematic_chain two_dof_robot_c;
+extern struct kca_kinematic_chain two_dof_robot_a;
 
 #ifdef __cplusplus
 }
